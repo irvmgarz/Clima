b96ce07e7aecfbc8ffe3b8e722f7c0a0c6cd1f58
irvmgarz <83097922+irvmgarz@users.noreply.github.com>
2023-06-21 13:52:13 -0500

    porfavorsubete

---
+++ js/funcion.js
@@ -0,0 +1,138 @@
+let plot = (data) => {
+  const ctx = document.getElementById("myChart");
+
+  const dataset = {
+    labels: data.hourly.time /* ETIQUETA DE DATOS */,
+    datasets: [
+      {
+        label: "Temperatura semanal" /* ETIQUETA DEL GRÁFICO */,
+        data: data.hourly.apparent_temperature /* ARREGLO DE DATOS */,
+        fill: false,
+        borderColor: "rgb(75, 192, 192)",
+        tension: 0.1,
+      },
+    ],
+  };
+  const config = {
+    type: "line",
+    data: dataset,
+  };
+  const chart = new Chart(ctx, config);
+};
+
+let plot2 = (data) => {
+  const ctx = document.getElementById("myChart2");
+
+  const dataset = {
+    labels: data.daily.time /* ETIQUETA DE DATOS */,
+    datasets: [
+      {
+        label: "Temperatura semanal" /* ETIQUETA DEL GRÁFICO */,
+        data: data.daily.uv_index_max /* ARREGLO DE DATOS */,
+        fill: false,
+        borderColor: "rgb(75, 192, 192)",
+        tension: 0.1,
+      },
+    ],
+  };
+  const config = {
+    type: "bar",
+    data: dataset,
+  };
+  const chart = new Chart(ctx, config);
+};
+
+let load = (data) => {
+  let URL =
+    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=apparent_temperature&daily=uv_index_max&timezone=auto";
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let timezone = data["timezone"];
+      let location = data["location"];
+      let latitude = data["latitude"];
+      let elevation = data["elevation"];
+
+      // document.getElementById("timezone").innerText = timezone;
+      // document.getElementById("location").innerText = location;
+      // document.getElementById("latitude").innerText = latitude;
+      // document.getElementById("elevation").innerText = elevation;
+    })
+    .catch(console.error);
+};
+
+let loadInocar = () => {
+  let URL_proxy = 'http://localhost:8080/' // Coloque el URL de acuerdo con la opción de proxy
+  let URL = URL_proxy + 'https://www.inocar.mil.ec/mareas/consultan.php';
+
+  fetch(URL)
+    .then((response) => response.text())
+    .then((data) => {
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(data, "text/html");
+      console.log(xml);
+      let contenedorMareas = xml.getElementsByTagName("div")[0];
+      let contenedorHTML = document.getElementById("table-container")
+      contenedorHTML.innerHTML = contenedorMareas.innerHTML;
+    })
+    .catch(console.error);
+};
+
+// (function () {
+//   fetch(URL)
+//     .then((response) => response.text())
+//     .then((data) => {
+//       const parser = new DOMParser();
+//       const xml = parser.parseFromString(data, "text/html");
+//       console.log(xml);
+//     })
+//     .catch(console.error);
+// })();
+
+// (function () {
+//   let meteo = localStorage.getItem("meteo");
+//   if (meteo == null) {
+//     let URL =
+//       "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=apparent_temperature&daily=uv_index_max&timezone=auto";
+
+//     fetch(URL)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         load(data);
+
+//         /* GUARDAR DATA EN LA MEMORIA */
+//         localStorage.setItem("meteo", JSON.stringify(data));
+//       })
+//       .catch(console.error);
+//   } else {
+//     /* CARGAR DATA DESDE LA MEMORIA */
+//     load(JSON.parse(meteo));
+//   }
+// })();
+
+(function () {
+  // const timezone = document.getElementById("timezone")
+  // const location = document.getElementById("location")
+  let URL =
+    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=apparent_temperature&daily=uv_index_max&timezone=auto";
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let timezone = data["timezone"];
+      let location = data["location"];
+      let latitude = data["latitude"];
+      let elevation = data["elevation"];
+
+      // document.getElementById("timezone").innerText = timezone;
+      // document.getElementById("location").innerText = location;
+      // document.getElementById("latitude").innerText = latitude;
+      // document.getElementById("elevation").innerText = elevation;
+
+      plot(data);
+      plot2(data);
+      loadInocar();
+    })
+    .catch(console.error);
+})();
